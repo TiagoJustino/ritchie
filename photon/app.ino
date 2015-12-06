07bbc364bfe57ed640972ffaf8351f77748f914a
Tiago Justino <tiago.vmj@gmail.com>
2015-12-06 15:05:55 -0500

    shake detection.

--- photon/app.ino
+++ photon/app.ino
@@ -1,6 +1,10 @@
 #include "MMA8452Q.h"
 #include <math.h>
+
+// at 25ms rate, 80 positions store 2s.
 #define ACCEL_QUEUE_SIZE 20
+
+#define SHAKE_TRESHOLD 1500000
 
 Servo servo;
 
@@ -11,6 +15,8 @@ int servoPin = D3;
 int servoPos = 180;
 int ledState = HIGH;
 
+boolean shaking = false;
+
 unsigned long now;
 unsigned long ledCounter;
 
@@ -18,7 +24,7 @@ int accelQueuePos = 0;
 
 struct {
   int x = 0, y = 0, z = 0;
-} previousAccel, accelQueue[ACCEL_QUEUE_SIZE];
+} accelQueue[ACCEL_QUEUE_SIZE];
 
 unsigned long readAccelCounter;
 unsigned long ledSignalingCounter;
@@ -57,28 +63,81 @@ void toggleLed() {
   servo.write(servoPos);
 }
 
-int getXAverage() {
-  int sum = 0;
+double getXMean() {
+  double sum = 0.0;
   for(int i = 0; i < ACCEL_QUEUE_SIZE; i++) {
     sum += accelQueue[i].x;
   }
-  return sum / ACCEL_QUEUE_SIZE;
+  return sum / ((double) ACCEL_QUEUE_SIZE);
 }
 
-int getYAverage() {
-  int sum = 0;
+double getYMean() {
+  double sum = 0.0;
   for(int i = 0; i < ACCEL_QUEUE_SIZE; i++) {
     sum += accelQueue[i].y;
   }
-  return sum / ACCEL_QUEUE_SIZE;
+  return sum / ((double) ACCEL_QUEUE_SIZE);
 }
 
-int getZAverage() {
-  int sum = 0;
+double getZMean() {
+  double sum = 0.0;
   for(int i = 0; i < ACCEL_QUEUE_SIZE; i++) {
     sum += accelQueue[i].z;
   }
-  return sum / ACCEL_QUEUE_SIZE;
+  return sum / ((double) ACCEL_QUEUE_SIZE);
+}
+
+double getXVariance() {
+  double sum = 0.0;
+  double mean = getXMean();
+  double dist;
+  for(int i = 0; i < ACCEL_QUEUE_SIZE; i++) {
+    dist = mean - accelQueue[i].x;
+    sum += dist * dist;
+  }
+  return sum / ((double) ACCEL_QUEUE_SIZE);
+}
+
+double getYVariance() {
+  double sum = 0.0;
+  double mean = getYMean();
+  double dist;
+  for(int i = 0; i < ACCEL_QUEUE_SIZE; i++) {
+    dist = mean - accelQueue[i].y;
+    sum += dist * dist;
+  }
+  return sum / ((double) ACCEL_QUEUE_SIZE);
+}
+
+double getZVariance() {
+  double sum = 0.0;
+  double mean = getZMean();
+  double dist;
+  for(int i = 0; i < ACCEL_QUEUE_SIZE; i++) {
+    dist = mean - accelQueue[i].z;
+    sum += dist * dist;
+  }
+  return sum / ((double) ACCEL_QUEUE_SIZE);
+}
+
+void checkShake() {
+  int xShake, yShake, zShake;
+  char tmp[128];
+  xShake = getXVariance() > SHAKE_TRESHOLD;
+  yShake = getYVariance() > SHAKE_TRESHOLD;
+  zShake = getZVariance() > SHAKE_TRESHOLD;
+  sprintf(tmp, "%f %f %f", getXVariance(), getYVariance(), getZVariance());
+  if(xShake || yShake || zShake) {
+    if(!shaking) {
+      Serial.println("Started Shaking.");
+      shaking = true;
+    }
+  } else {
+    if(shaking) {
+      Serial.println("Stopped Shaking.");
+      shaking = false;
+    }
+  }
 }
 
 void loop() {
@@ -87,9 +146,7 @@ void loop() {
   if(now - readAccelCounter >= 25) {
     readAccelCounter = now;
     readAccel();
-    previousAccel.x = accel.x;
-    previousAccel.y = accel.y;
-    previousAccel.z = accel.z;
+    checkShake();
   }
 
   if(now - ledCounter >= ledFrequency) {
