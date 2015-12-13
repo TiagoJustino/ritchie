31c74869e6e8322613c13e306dcb270d0ea221b0
Tiago Justino <tiago.vmj@gmail.com>
2015-12-12 19:33:11 -0500

    melodies added.

---
+++ photon/rocky.h
@@ -0,0 +1,18 @@
+#ifndef ROCKY_H
+#define ROCKY_H
+#include "pitches.h"
+
+int rockyMelodyLength = 35;
+
+int rockyMelody[] = { NOTE_B3,NOTE_D4,0,NOTE_E4,  NOTE_E4,NOTE_FS4,0,NOTE_B3,
+  NOTE_B3,NOTE_D4,0,NOTE_E4,  NOTE_E4,NOTE_FS4,0,NOTE_B3,0,
+  NOTE_A3,NOTE_G3,NOTE_A3,NOTE_G3,NOTE_A3,NOTE_B3,0,
+  NOTE_G3,NOTE_G3,NOTE_G3,NOTE_G3,NOTE_G3,NOTE_G3,NOTE_G3,NOTE_G3,NOTE_G3,NOTE_C4,
+  NOTE_B3 };
+
+int rockyNoteDurations[] = {
+  16,12,12,1.5,  16,12,12,1.5,  16,12,12,1.5,  16,12,12,1,12,
+  16,16,12,16,16,3,12,  16,16,8,16,16,8,16,16,8,8, 
+};
+
+#endif
---
+++ photon/unknownsong.h
@@ -0,0 +1,15 @@
+#ifndef UNKOWN_SONG_H
+#define UNKOWN_unknownSong_H
+#include "pitches.h"
+
+int unknownSongMelodyLength = 9;
+
+int unknownSongMelody[] = {
+  NOTE_C4, NOTE_G3,NOTE_G3, NOTE_A3, NOTE_G3, REST, NOTE_B3, NOTE_C4, REST
+};
+
+int unknownSongNoteDurations[] = {
+  8,16,16,8,8,8,8,8,8
+};
+
+#endif
